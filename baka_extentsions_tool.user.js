a3b71cfceb2972e77793d368cd3d24f045ed07f3
Albert Safin <xzfcpw@gmail.com>
2015-05-28 10:22:17 +0600

    Code style

--- baka_extentsions_tool.user.js
+++ baka_extentsions_tool.user.js
@@ -23,15 +23,14 @@
     var serverRestart = false
 
     var defaultName = "Безымянная сырно"
-    function isAlly(name) { return window.bakaconf.allyNameRe.test(name) || (window.bakaconf.allyNames.indexOf(name) > -1) }
     function g(id) {return document.getElementById(id)}
-    
+
     function setConf(defaults) {
         if(window.bakaconf === undefined)
-            window.bakaconf = {};
+            window.bakaconf = {}
         for(var i in defaults)
             if (window.bakaconf[i] === undefined)
-                window.bakaconf[i] = defaults[i];
+                window.bakaconf[i] = defaults[i]
     }
 
     function join(l) {
@@ -54,38 +53,38 @@
         t = new Date(t*1000 + 1000*60*60*3)
         var h = pad(t.getUTCHours()), m = pad(t.getUTCMinutes()), s = pad(t.getUTCSeconds())
         switch(currentTimeFormat % 3) {
-            case 0: return  h+':'+m+':'+s+' ';
-            case 1: return  h+':'+m+' ';
-            case 2: return '';
+        case 0: return  h+':'+m+':'+s+' '
+        case 1: return  h+':'+m+' '
+        case 2: return ''
         }
     }
     function reformatTime() {
         currentTimeFormat ++
-        var l = document.getElementsByClassName("time");
+        var l = document.getElementsByClassName("time")
         for(var i = 0; i < l.length; i++)
             l.item(i).textContent = formatTime(l.item(i).getAttribute("T"))
     }
-    
+
     var chatHidden = false
     function chatHider() {
         chatHidden = !chatHidden
         g('cbox').style.visibility = (chatHidden ? 'hidden' : '')
         updateNotification()
     }
-    
+
     var mapHidden = false
     function mapHider() {
         mapHidden = !mapHidden
         g('map').style.visibility = (mapHidden ? 'hidden' : '')
     }
-    
+
     var unreadCount = 0
     function updateNotification() {
         var n = g("notification")
         if (!chatHidden) {
             unreadCount = 0
             n.style.visibility =  'hidden'
-        } else {        
+        } else {
             if(unreadCount) {
                 n.textContent = unreadCount
                 n.style.visibility =  ''
@@ -94,20 +93,20 @@
             }
         }
     }
-    
+
     var chatUsersCount = 0
     function setChatUsersCount(add, value) {
         if (add)
-            chatUsersCount += value;
+            chatUsersCount += value
         else
-            chatUsersCount = value;
-        
+            chatUsersCount = value
+
         g("chat_users").textContent = (chatUsersCount >= 0) ? chatUsersCount : "#"
     }
 
     function connectChat() {
         function reconnectButton(e) {
-            e = e || window.event;e = e.target || e.srcElement;
+            e = e || window.event;e = e.target || e.srcElement
             e.parentNode.removeChild(e)
             connectChat()
         }
@@ -130,51 +129,51 @@
         websocket.onmessage = function(evt) {
             var d = JSON.parse(evt.data)
             switch(d.t) {
-                case "names":
-                    d.names = d.names.map(function(n) { return typeof n == "string" ? n : n.name })
-                    var namesList = d.names.filter(function(n) { return n !== "" }).map(aName)
-                    var nonameCount = d.names.length - namesList.length
-                    var iHaveNoName = g('nick').value === ""
-                    if(nonameCount === 0) / * do nothing * /;
-                    else if(nonameCount === 1)
-                        namesList.push("одна безымянная сырно" + (iHaveNoName?" (это ты)":""))
-                    else
-                        namesList.push(nonameCount + " безымянных сырно" + (iHaveNoName?" (включая тебя)":""))
-                    addLine({time:d.T, message: [].concat(["В чате "], join(namesList), ["."])})
-                    setChatUsersCount(false, d.names.length)
-                    break;
-                case "message":
-                    addLine({time:d.T, sender:d.f, message:formatMessage(d.text)})
-                    unreadCount += 1;updateNotification()
-                    break;
-                case "quick":
-                    addLine({time:d.T, sender:d.f, message:"[" + d.text + "]"})
-                    unreadCount += 1;updateNotification()
-                    break;
-                case "name":
-                    addLine({time:d.T, message: [aName(d.f), " теперь ", aName(d.name), "."]})
-                    break;
-                case "map":
-                    drawMap(d.data)
-                    break;
-                case "join":
-                    if (d.f !== "")
-                        addLine({time:d.T, message: [aName(d.f), " заходит."]});
-                    setChatUsersCount(true, +1)
-                    break;
-                case "leave":
-                    if (d.f !== "")
-                        addLine({time:d.T, message: [aName(d.f), " выходит."]});
-                    setChatUsersCount(true, -1)
-                    break;
-                case "ping":
-                    d.t = 'pong'
-                    send(d)
-                    break;
-                case "restart":
-                    addLine({time:d.T, message:["Сейчас сервер будет перезапущен"]})
-                    serverRestart = true
-                    break;
+            case "names":
+                d.names = d.names.map(function(n) { return typeof n == "string" ? n : n.name })
+                var namesList = d.names.filter(function(n) { return n !== "" }).map(aName)
+                var nonameCount = d.names.length - namesList.length
+                var iHaveNoName = g('nick').value === ""
+                if(nonameCount === 0) {/* do nothing */}
+                else if(nonameCount === 1)
+                    namesList.push("одна безымянная сырно" + (iHaveNoName?" (это ты)":""))
+                else
+                    namesList.push(nonameCount + " безымянных сырно" + (iHaveNoName?" (включая тебя)":""))
+                addLine({time:d.T, message: [].concat(["В чате "], join(namesList), ["."])})
+                setChatUsersCount(false, d.names.length)
+                break
+            case "message":
+                addLine({time:d.T, sender:d.f, message:formatMessage(d.text)})
+                unreadCount += 1;updateNotification()
+                break
+            case "quick":
+                addLine({time:d.T, sender:d.f, message:"[" + d.text + "]"})
+                unreadCount += 1;updateNotification()
+                break
+            case "name":
+                addLine({time:d.T, message: [aName(d.f), " теперь ", aName(d.name), "."]})
+                break
+            case "map":
+                drawMap(d.data)
+                break
+            case "join":
+                if (d.f !== "")
+                    addLine({time:d.T, message: [aName(d.f), " заходит."]})
+                setChatUsersCount(true, +1)
+                break
+            case "leave":
+                if (d.f !== "")
+                    addLine({time:d.T, message: [aName(d.f), " выходит."]})
+                setChatUsersCount(true, -1)
+                break
+            case "ping":
+                d.t = 'pong'
+                send(d)
+                break
+            case "restart":
+                addLine({time:d.T, message:["Сейчас сервер будет перезапущен"]})
+                serverRestart = true
+                break
             }
         }
     }
@@ -199,9 +198,9 @@
         a.click()
         document.body.removeChild(a)
     }
-    
+
     function clickName(e) {
-        e = e || window.event;e = e.target || e.srcElement;
+        e = e || window.event;e = e.target || e.srcElement
         var ca = document.getElementById('carea')
         ca.value = e.textContent + ": " + ca.value
         ca.focus()
@@ -224,7 +223,7 @@
 
     function formatMessage(text) {
         var addrRe = /(ws:\/\/\d+\.\d+\.\d+\.\d+:\d+)/
-        var i, node, result = text.split(addrRe)
+            var i, node, result = text.split(addrRe)
         if (text.indexOf(g('nick').value||defaultName) > -1)
             result.higlight = true
         for (i = 1; i < result.length; i += 2)
@@ -234,7 +233,7 @@
 
     function addLine(p) {
         var d = document.createElement('div')
-        
+
         if(p.time !== undefined) {
             var time = document.createElement('span')
             time.className = "time"
@@ -273,13 +272,13 @@
             websocket.send(JSON.stringify(a))
     }
     window.send = send
-    
+
     function sendAddr() {
         if(window.agar === undefined ||
            window.agar.ws === undefined ||
            window.agar.top === undefined ||
            window.agar.top.length === 0)
-            return false;
+            return false
         var ws = window.agar.ws
         if (ws[ws.length-1] == '/')
             ws = ws.substring(0, ws.length-1)
@@ -290,27 +289,26 @@
 
     function submit(e) {
         var ca = document.getElementById('carea')
-        
+
         if(ca.value != "") {
             var n = document.getElementById('nick')
             if (myName != n.value) {
                 myName = n.value
                 send({t: "name", name:myName})
             }
-            if(ca.value[0] == "/") {
+            if(ca.value[0] == "/")
                 switch(ca.value) {
-                    case "/names":
-                        send({t:"names"}); break;
-                    case "/addr":
-                        sendAddr(); break;
-                    default:
-                        addLine({message: ["Команды чата:"]})
-                        addLine({message: ["/names — получить список сырн в чате"]})
-                        addLine({message: ["/addr — отправить текущий севрер и топ (требуется expose)"]})
+                case "/names":
+                    send({t:"names"}); break
+                case "/addr":
+                    sendAddr(); break
+                default:
+                    addLine({message: ["Команды чата:"]})
+                    addLine({message: ["/names — получить список сырн в чате"]})
+                    addLine({message: ["/addr — отправить текущий севрер и топ (требуется expose)"]})
                 }
-            } else {
+            else
                 send({t: "message", text: ca.value})
-            }
             ca.value = ""
             ca.blur()
         }
@@ -330,24 +328,24 @@
         var extended = false
         window.onkeydown = function(e) {
             if(extended) {
-                if(e.keyCode == 16) return false;
+                if(e.keyCode == 16) return false
                 var cmd = window.bakaconf.quickTemplates[e.keyCode - 49]
                 if (cmd !== undefined) {
-                    cmd = cmd[e.shiftKey?1:0];
+                    cmd = cmd[e.shiftKey?1:0]
                     if (cmd !== undefined)
-                        send({t:"quick", text:cmd});
+                        send({t:"quick", text:cmd})
                 }
                 quickHint.style.visibility = 'hidden'
                 extended = false
                 return false
             }
-            
+
             if (chatactive)
                 if (e.keyCode == 27 || e.keyCode == 9) {
                     g('carea').blur()
                     return false
-                } else return true;
-            
+                } else return true
+
             if (e.ctrlKey && e.keyCode === 83) {
                 downloadTopScreenshot()
                 return false
@@ -355,30 +353,31 @@
 
             if(!e.altKey && !e.shiftKey && !e.ctrKey && !e.metaKey) {
                 switch(e.keyCode) {
-                    case 9: g('carea').focus(); return false;
-                    case 49: chatHider(); return true;
-                    case 50: reformatTime(); return true;
-                    case 51: move(); return true;
-                    case 52: extended = true; quickHint.style.visibility = ''; return true;
-                    case 53: mapHider(); return true;
-                    case 81: repeat = 1; return true;
+                case 9: g('carea').focus(); return false
+                case 49: chatHider(); return true
+                case 50: reformatTime(); return true
+                case 51: move(); return true
+                case 52: extended = true; quickHint.style.visibility = ''; return true
+                case 53: mapHider(); return true
+                case 81: repeat = 1; return true
                 }
             }
-            return olddown(e);
-        };
+            return olddown(e)
+        }
         window.onkeyup = function(e) {
             switch(e.keyCode) {
-                case 81: repeat = 0;break;
-                default: return oldup(e)
+            case 81: repeat = 0;break
+            default: return oldup(e)
             }
-        };
-        var k = {keyCode: 87};
+        }
+        var k = {keyCode: 87}
         setInterval(function() {
-            if (!repeat) return;
-            olddown(k);oldup(k);
+            if (!repeat) return
+            olddown(k)
+            oldup(k)
         }, 50)
     }
-    
+
     function handleSetNick() {
         var oldSetNick = window.setNick
         window.setNick = function(n) {
@@ -389,12 +388,12 @@
             oldSetNick(n)
         }
     }
-    
+
     function sendMap() {
         var a = window.agar
         if (a === undefined || a.allCells === undefined || a.myCells === undefined || a.top === undefined || a.ws === "") {
             if (!mapHidden)
-                send({t:'map', reply:1});
+                send({t:'map', reply:1})
             return
         }
         var cells = a.allCells.filter(function(c){
@@ -411,27 +410,31 @@
         var top = a.top.map(function(x){return [x.id, x.name]})
         send({t:'map', all:cells, my:a.myCells, top:top, reply:mapHidden?0:1})
     }
-    
+
     function sendMapThread() {
         if (window.agar === undefined)
-            addLine({message:["Карта отправляться не будет :<"]});
+            addLine({message:["Карта отправляться не будет :<"]})
         setInterval(sendMap, 1000)
     }
-    
+
     function drawMap(data) {
+        function isAlly(name) {
+            return window.bakaconf.allyNameRe.test(name) ||
+                (window.bakaconf.allyNames.indexOf(name) > -1)
+        }
         if (mapHidden)
-            return;
+            return
         var map = document.getElementById("map")
         var context = map.getContext('2d')
         var myCells = ((window.agar || {}).myCells) || []
-        
+
         context.clearRect(0 , 0, canvas.width, canvas.height)
         context.globalAlpha = 0.5
         context.fillStyle = "#777"
         context.fillRect(0 , 0, canvas.width, canvas.height)
         var scale = 256/11180
         var i
-        
+
         context.globalAlpha = .4
         for(i = 0; i < data.length; i++) {
             var a = data[i].a
@@ -440,15 +443,17 @@
             if(a || m || t) {
                 context.fillStyle = m ? "#fff" : (a ? "#000" :  "#00f")
                 context.beginPath()
-                context.arc(data[i].x * scale, data[i].y * scale, data[i].s*scale+4, 0, 2 * Math.PI, false)
+                context.arc(data[i].x*scale, data[i].y*scale,
+                            data[i].s*scale+4, 0, 2 * Math.PI, false)
                 context.fill()
             }
         }
-        
+
         context.lineWidth = 2
         for(i = 0; i < data.length; i++) {
             context.beginPath()
-            context.arc(data[i].x * scale, data[i].y * scale, data[i].s*scale, 0, 2 * Math.PI, false)
+            context.arc(data[i].x*scale, data[i].y*scale,
+                        data[i].s*scale, 0, 2 * Math.PI, false)
             context.globalAlpha = 1
             context.fillStyle = data[i].c
             context.fill()
@@ -465,7 +470,7 @@
 
     function init() {
         var stl = document.createElement('style')
-        stl.textContent = '#cbox {background: black; position:fixed; z-index:205; bottom:0; right:0; width:400px; opacity:0.5; color:white;} ' +
+        stl.textContent = '#cbox {background: black; position:fixed; z-index:205; bottom:0; right:0; width:400px; opacity:0.5; color:white;}' +
             '#carea { width: 100%; color: black}' +
             '#form {margin: 0;}' +
             '#msgsbox { overflow: auto; word-wrap: break-word; width:400px; height: 250px; }' +
@@ -475,38 +480,38 @@
             '#notification {background: red; position:fixed; z-index:205; bottom:5px;right:5px;opacity:0.5;color:white}'+
             '#quickHint {background: #777; position:fixed; z-index:120; top:0; left:0; color:white;}'+
             '#quickHint .key {font-weight: bold;margin-right: 1em;}'+
-            '#map {position: fixed; bottom: 5px; left: 5px; z-index:205; border: 1px black solid;}';
+            '#map {position: fixed; bottom: 5px; left: 5px; z-index:205; border: 1px black solid;}'
         document.body.appendChild(stl)
 
         var cbox = document.createElement('table')
         cbox.cellpadding = cbox.cellspacing = 0
         cbox.id = 'cbox'
         cbox.innerHTML = '<tr><td colspan="2"><div id="msgsbox"></div></td></tr>' +
-            '<tr height="0">' + 
+            '<tr height="0">' +
             '<td width="100%"><form id="form"><input id="carea" autocomplete="off"></input></form></td>' +
             '<td id="chat_users"></td>' +
-            '</tr>';
+            '</tr>'
         document.body.appendChild(cbox)
-        
+
         var notification = document.createElement('div')
         notification.id = "notification"
         document.body.appendChild(notification)
-        
+
         var quickHint = document.createElement('div')
         quickHint.id = "quickHint"
         var addQuickHint = function (key, text) {
             var line = document.createElement('div')
-            
+
             var key_ = document.createElement('span')
             key_.textContent = key
             key_.className = 'key'
             line.appendChild(key_)
-            
+
             var text_ = document.createElement('span')
             text_.textContent = text
             text_.className = 'text'
             line.appendChild(text_)
-            
+
             quickHint.appendChild(line)
         }
         for(var i = 0; i < window.bakaconf.quickTemplates.length; i++) {
@@ -515,7 +520,7 @@
         }
         quickHint.style.visibility = 'hidden'
         document.body.appendChild(quickHint)
-        
+
         var map = document.createElement("canvas")
         map.id = "map"
         map.width = 256
@@ -531,20 +536,22 @@
             chatactive = false
             g('cbox').style.opacity = '0.5'
         }
-        
+
         g("chat_users").onclick = function() { send({t:'names'}) }
-        
+
         handleSetNick()
         handleKeys()
         connectChat()
         sendMapThread()
-        map.onmousemove = notification.onmousemove = cbox.onmousemove = g("canvas").onmousemove
+        map.onmousemove = notification.onmousemove = cbox.onmousemove =
+            g("canvas").onmousemove
         notification.onclick = chatHider
     }
 
     function wait() {
-        if (!window.onkeydown || !window.onkeyup || !window.setNick || !g("canvas").onmousemove)
-            return setTimeout(wait, 100);
+        if (!window.onkeydown || !window.onkeyup ||
+            !window.setNick || !g("canvas").onmousemove)
+            return setTimeout(wait, 100)
         init()
     }
     wait()
