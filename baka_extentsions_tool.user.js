xzfc/baka-extensions-tool

commit de7ce546e857e23aad5ef6f22fff7febab678985

Add chat input history navigation

baka_extentsions_tool.user.js
@@ -185,6 +185,12 @@
                 chat.active = false
                 g('cbox').style.opacity = '0.7'
             }
+            g('carea').onkeydown = function(e) {
+                switch (e.keyCode) {
+                case 38: return submitHistory.up(), false
+                case 40: return submitHistory.down(), false
+                }
+            }
         },
         toggle: function(show) {
             this.hidden = show === undefined ? !this.hidden : !show
@@ -693,6 +699,31 @@
         })
     }
 
+    var submitHistory = {
+        list: [],
+        idx: -1,
+        text: "",
+        up: function() {
+            if (this.idx == -1) {
+		this.text = g('carea').value
+		g('carea').value = this.list[this.idx = this.list.length-1]
+	    } else if (this.idx != 0)
+		g('carea').value = this.list[--this.idx]
+        },
+        down: function() {
+            if (this.idx == this.list.length-1) {
+		this.idx = -1
+		g('carea').value = this.text
+	    } else if (this.idx != -1)
+		g('carea').value = this.list[++this.idx]
+        },
+        push: function(t) {
+            this.idx = -1
+            if (this.list[this.list.length -1] !== t)
+                this.list.push(t)
+        }
+    }
+
     function submit(e) {
         var ca = document.getElementById('carea')
         function sendName() {
@@ -751,6 +782,7 @@
                 sendName()
                 send({t: "message", text: ca.value})
             }
+            submitHistory.push(ca.value)
             ca.value = ""
             if (window.agar === undefined || window.agar.myCells === undefined || window.agar.myCells.length !== 0)
                 ca.blur()

commit 54221dc57a9bcb99bebded9ced4f8a0a922e9cd0

[BUGFIX] When ↑↓ pressed, ensure that chat history is non-empty
Bug: When there are no chat history, pressing ↑ puts `undefined` in carea, and pressing ↓ clears entered text.

This bug was introduced in de7ce546e857e23aad5ef6f22fff7febab678985

baka_extentsions_tool.user.js
@@ -707,6 +707,8 @@
         idx: -1,
         text: "",
         up: function() {
+            if (this.list.length === 0)
+                return
             if (this.idx == -1) {
 		this.text = g('carea').value
 		g('carea').value = this.list[this.idx = this.list.length-1]
@@ -714,6 +716,8 @@
 		g('carea').value = this.list[--this.idx]
         },
         down: function() {
+            if (this.list.length === 0)
+                return
             if (this.idx == this.list.length-1) {
 		this.idx = -1
 		g('carea').value = this.text