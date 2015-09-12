79b19a4d9e587361c938b8eccef76f27a677b54a
Albert Safin <xzfcpw@gmail.com>
2015-09-13 01:57:02 +0600

    Bump version

--- baka_extentsions_tool.user.js
+++ baka_extentsions_tool.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name        Baka extensions tool
-// @version     1.26.2
+// @version     1.27
 // @namespace   baka-extensions-tool
 // @updateURL   https://raw.githubusercontent.com/xzfc/baka-extensions-tool/master/baka_extentsions_tool.user.js
 // @include     http://agar.io/*
@@ -10,7 +10,7 @@
 // ==/UserScript==
 
 (function() {
-    var version = "1.26.2"
+    var version = "1.27"
     setConf({wsUri: "ws://89.31.114.117:8000/",
              quickTemplates: {
                  _192: {
